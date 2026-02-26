fdf977202f1409f584ac53360cb869344efe0f12
aprox <giedrius.punis@gmail.com>
2026-02-27 01:51:58 +0200

    add port_definition obj

---
+++ src/port_definitions.py
@@ -0,0 +1,16 @@
+from typing import NamedTuple
+
+class PortRange(NamedTuple):
+    start: int
+    end: int
+
+class PortConfig(NamedTuple):
+    standard: PortRange
+    services: PortRange
+    private: PortRange
+
+PORT_RANGES = PortConfig(
+    standard=PortRange(0, 1023),
+    services=PortRange(1024, 49151),
+    private=PortRange(49152, 65535)
+)
