b492df9487ed514e76d720a968c0e8aec162794f
aprox <giedrius.punis@gmail.com>
2026-02-27 04:29:17 +0200

    mvp

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
-import socket
-from src.port_definitions import PORT_RANGES, PortRange
+import asyncio
+import ipaddress
+from src.port_definitions import PORT_RANGES
+from src.scan_utils import scan_port_range
 
-
-SOCK_TIMEOUT_MS = 1
 OPEN_PORTS: list[int] = []
 
 
-def cache_port(port: int, cache: list[int] = OPEN_PORTS) -> None:
-    assert isinstance(port, int)
-    cache.append(port)
-    
-    
-def check_port(tartget_ip: str, port: int) -> int | None:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(SOCK_TIMEOUT_MS)
-    
-    result = sock.connect_ex((tartget_ip, port))
-    if result == 0:
-        return port 
-    
-    return None
+async def main() -> None:
+	target_ip = None
+
+	while target_ip is None:
+		user_input = input("Enter target IPv4 address: ").strip()
+
+		if validate_ip(user_input):
+			target_ip = user_input
+		else:
+			print("Invalid IPv4 address.", flush=True)
+
+	for item in PORT_RANGES:
+		await scan_port_range(item, target_ip, cache_port)
+
+	print(f"\nOpen ports: {OPEN_PORTS}")
 
 
-def scan_port_range(port_range: PortRange, target: str, cache: function) -> None:
-    for port in range(port_range):
-        result = check_port(target, port)
-        
-        if (result):
-            cache(port)
-        
+def cache_port(port: int, cache: list[int] | None = None) -> None:
+	if cache is None:
+		cache = OPEN_PORTS
 
-def main():
-    target_ip = input('Enter target IP: ')
-    
-    for item in PORT_RANGES:
-        scan_port_range(item, target_ip, cache_port)
-        
-    print(f"Open ports: {OPEN_PORTS}", end="")
-    
+	cache.append(port)
+
+
+def validate_ip(target: str) -> bool:
+	try:
+		ipaddress.IPv4Address(target)
+		return True
+	except ipaddress.AddressValueError:
+		return False
+
 
 if __name__ == "__main__":
-    main()
+	try:
+		asyncio.run(main())
+	except KeyboardInterrupt:
+		print("\nScan cancelled by user.")
+	except Exception as e:
+		print(f"\nAn error occurred: {e}")
